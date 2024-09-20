6f26803d66d4b28d1f3a4a62834f746efdc5a11d
noureennaaz <noureennaaz09@gmail.com>
2024-09-20 08:08:16 +0530

    adding the 3d header

--- src/App.js
+++ src/App.js
@@ -25,15 +25,15 @@ function App() {
     deleteSpeed: 20,
   });
   return (
-    <div className="min-h-screen relative text-white ">
-    <img src={orange} className="absolute top-0 right-0 opacity-70 h-72 z-0 rotate-180" alt=""/>
+    <div className="min-h-screen relative text-white overflow-hidden">
+    {/* <img src={orange} className="absolute top-0 right-0 opacity-70 h-72 z-0 rotate-180" alt=""/> */}
     {/* <img src={shape} className="absolute left-60 opacity-70 top-60 -rotate-180 lg:h-[400px] md:h-[300px] z-0 max-md:hidden max-lg:left-40 " alt=""/> */}
    
  
 
       
-      <div className="py-20  relative bg-[#B3AFE9] overflow-hidden lg:h-[78vh]">
-      <Spline scene="https://prod.spline.design/96kt3R9OLanMMHDs/scene.splinecode" className="w-screen absolute sm-left-0 lg:-left-24 z-0 sm:-top-40  lg:h-[78vh] overflow-hidden" />
+      <div className="lg:pt-20 md:pt-20 pt-12  relative bg-[#B3AFE9] lg:h-[78vh] md:h-[90vh] ">
+      <Spline scene="https://prod.spline.design/96kt3R9OLanMMHDs/scene.splinecode" className="w-screen absolute lg:-left-48 md:-left-40 sm:-left-48 z-0 lg:h-[78vh] h-[90vh] sm:translate-y-0 translate-y-20 overflow-hidden sm:mt-0 -mt-32" />
         <div className="flex items-center max-md:flex-wrap-reverse mx-auto w-4/6 justify-between gap-10 lg:mb-40  ">
           <div className=" relative mt-5">
             
@@ -41,8 +41,8 @@ function App() {
           </div>
           <div className="w-max flex flex-col gap-20 shadow-text">
             
-            <div className="lg:overflow-visible z-50 pt-20 ">
-            <h1 className="text-[48px] playpen-sans-700 h-36 max-md:h-fit max-md:overflow-hidden lg:overflow-visible">
+            <div className="lg:overflow-visible z-50 lg:pt-20 ">
+            <h1 className="lg:text-[48px] md:text-[30px] leading-[40px]  lg:leading-[66px] text-3xl z-50  playpen-sans-700 h-36 max-md:h-fit max-md:overflow-hidden lg:overflow-visible">
               Hey!
               <br />I am
               <span className="text-emerald-200 font-semibold">
@@ -61,14 +61,16 @@ function App() {
             </p>
             </div>
             <div className="flex gap-20 justify-evenly w-full p-30">
-             <Share/>
+             <Share className='z-50'/>
           
         </div>
           </div>
         </div>
       </div>
-      <img src={waves} alt="" className="w-screen absolute -z-20 -translate-y-32"/>
-      <div className="mt-20  w-full relative cursor-default">
+      {/* <div className="relative h-80"> */}
+        <img src={waves} alt="" className="w-screen absolute -z-50"/>
+      {/* </div> */}
+      <div className="lg:mt-60 mt-40  w-full relative cursor-default">
         <Projects/>
         <img src={pc} className="absolute left-[50px] top-[500px] opacity-50 filter h-[500px] -z-20 " alt=""/>
         <img src={laptop} className="absolute right-[50px] top-[100px] opacity-50 filter h-[300px] -z-20 " alt=""/>
--- src/components/Share.js
+++ src/components/Share.js
@@ -1,11 +1,11 @@
 export default function Share() {
   const element = document.querySelector("#info")
   return (
-    <div className="w-full p-30 z-50 group">
+    <div className="w-full sm:mt-0 mt-[300px] min-h-fit pt-0 p-30 z-50 group">
     
-    <p className="">Let's Connect</p>
+    <p>Let's Connect</p>
     <div className="h-[2px] mt-3 mb-3 scale-0 transition duration-500 group-hover:scale-100 w-40 bg-gradient-to-r from-pink-500 to-yellow-500 "></div>
-      <ul className="example-2 -translate-x-1 flex flex-wrap max-md:justify-center max-md:items-center relative">
+      <ul className="example-2 -translate-x-1 flex flex-wrap max-md:justify-center max-md:items-center relative z-50">
         <li className="icon-content">
           <a
             href="https://www.linkedin.com/in/noureen-naaz-599392229/"
